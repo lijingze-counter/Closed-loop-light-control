cb18d556ed9d4b2514258af6b99fcc9411d6075c
lijingze-counter <62199283+lijingze-counter@users.noreply.github.com>
2022-06-28 20:17:45 +0800

    Add files via upload

---
+++ core/Inc/key.h
@@ -0,0 +1,14 @@
+#ifndef __KEY_H__
+#define __KEY_H__
+
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+
+int key_read();
+
+
+void key();
+
+#endif /*__ GPIO_H__ */
